b0ec4b5b81aaf2d59de8d64ecbd99b78b897ff1d
Srinandhan20 <107259556+Srinandhan20@users.noreply.github.com>
2022-07-10 15:42:15 +0530

    Add files via upload

---
+++ listandmethods.py
@@ -0,0 +1,37 @@
+# a=[1,2]
+# print(type(a))
+# b=[]
+# print(type(b))
+# p=['ameerpet',55]
+# print(p[0],type(p[0]))
+# a=None
+# print(type(a))
+
+# r=[4,8.65,'python']
+# print(r)
+# r[0]=80
+# r[-1]='program'
+# print(r)
+
+# n=[4,3.45,'hello',[2,3.34]]
+# print(n,type(n))
+# print(n[0],type(n[0]))
+# print(n[3],type([3]))
+
+# n=[1,3.4,'hey',[1,2,3.4]]
+# print(n)
+# n.append('josh')
+# print(n)
+
+# b=[2,33.4,'hey',[1,23.56,'hello']]
+# print(b)
+# b.clear()
+# print(b)
+
+n=[1,22,33.4,'python',1]
+print(n.count(1))
+print(n.count('python'))
+
+m=[2,4,6,8,'hello']
+print(m.index(4))
+print(m.index(8))
